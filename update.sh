aefe65d5f36d617997d6e26d941bdb3466b139c5
Aleytia <jarek13705@gmail.com>
2020-11-27 14:46:06 -0800

    Add autoremove of old image

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker rm -f "usagi"
+OLD_IMAGE_ID=$(docker images --filter=reference=usagi --format "{{.ID}}")
+docker rm -f usagi
 git pull
 ./build.sh
 ./start.sh
+docker rmi "$OLD_IMAGE_ID"
