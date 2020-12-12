4857f3bab24f2fce4b7115c4255721b43c072a99
Aleytia <jarek13705@gmail.com>
2020-12-12 15:37:02 -0800

    Fix missing quote in Google default

--- src/resolvers/google.py
+++ src/resolvers/google.py
@@ -1,7 +1,7 @@
 from urllib.parse import quote
 
 def default(args):
-    return 'https://www.google.com/search?q={}'.format(' '.join(args))
+    return 'https://www.google.com/search?q={}'.format(quote(' '.join(args)))
 
 def resolve(args=[]):
     if len(args) > 1:
